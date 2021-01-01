9246c85d8c237bc118efab18d012380e1c7f47b6
MohaM <s.mohammad.msv@gmail.com>
2021-01-01 18:52:49 +0330

    added to new repository

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+COMPILER=clang++
+BASE_DIR=$(dirname $0)
+BUILD_DIR=$BASE_DIR/build
+SRC_DIR=$BASE_DIR/libargparser
+
+if [ ! -d $BUILD_DIR ]; then
+    mkdir -p $BUILD_DIR &>/dev/null
+    if [ $? -ne 0 ]; then
+        echo "Could not create build directory" >&2
+        exit 1
+    fi
+fi
+
+$COMPILER \
+    -std=c++2a \
+    -fmodules-ts \
+    --precompile \
+    -o $BUILD_DIR/libargparser.pcm \
+    $SRC_DIR/libargparser.cppm
+
